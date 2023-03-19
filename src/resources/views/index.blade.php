ae38be58e11cd6632f3b9646862eca21a725f236
sho55 <gra2546nd@gmail.com>
2023-03-19 18:33:04 +0900

    :fire: 不要ファイル削除

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <meta name="keywords" content="スシロー,スシロー1000円ガチャ,1000円ガチャ,遊び,ランダム,メニュー">
+    <meta name="description" content="スシローのメニューをランダムで1000円以内で選びます。スシロー非公式アプリ。">
+
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script src="{{ asset('js/app.js') }}"></script>
     <title>スシロー1000円ガチャ</title>
